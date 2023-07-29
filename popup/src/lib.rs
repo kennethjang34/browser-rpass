1a2b51ebb79df391331abacb568308eddfac84c5
JANG <kennethjang34@gmail.com>
2023-07-29 10:45:31 -0700

    chore: add communication logic to popup

--- popup/src/lib.rs
+++ popup/src/lib.rs
@@ -1,12 +1,11 @@
-use reqwest_wasm::get;
-use std::f32::consts::E;
+use gloo_utils::format::JsValueSerdeExt;
+use std::collections::HashMap;
+use util::*;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-use web_sys::*;
 
 #[macro_use]
 mod util;
+
 mod api;
 mod app;
 mod components;
@@ -14,6 +13,21 @@ mod store;
 
 #[wasm_bindgen(start)]
 pub fn run_app() -> Result<(), JsValue> {
+    let mut hashmap = HashMap::new();
+    hashmap.insert("text".to_owned(), "dassd".to_owned());
+    let port = chrome.runtime().connect("binlemkoadegmiinejciimieplcjkkfo");
+    port.post_message(<JsValue as JsValueSerdeExt>::from_serde(&hashmap).unwrap());
+    let callback = Closure::<dyn Fn(JsValue, Port)>::new(move |msg: JsValue, port: Port| {
+        log!("message received at popup: {:?}", msg);
+        port.post_message(
+            <JsValue as JsValueSerdeExt>::from_serde(&format!(
+                "hello from popup! I have received message {:?}",
+                msg
+            ))
+            .unwrap(),
+        );
+    });
+    port.on_message().add_listener(callback.into_js_value());
     yew::Renderer::<app::App>::new().render();
     Ok(())
 }
