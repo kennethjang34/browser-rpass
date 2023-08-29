b711592a802dfebb9b5db0cc44aaf449900f737b
JANG <kennethjang34@gmail.com>
2023-08-28 20:07:31 -0700

    build: change service_worker to service-worker

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -rf ./pkg
 mkdir pkg
-wasm-pack build ./service_worker --target web --out-dir ./pkg
+wasm-pack build ./service-worker --target web --out-dir ./pkg
 wasm-pack build ./content --target web --out-dir ./pkg
 trunk build ./popup/index.html
 cp -r ./popup/dist/* ./pkg
@@ -8,8 +8,8 @@ cp ./init.js ./pkg
 cp ./run_service_worker.js ./pkg
 cp ./run_wasm_content.js ./pkg
 cp ./run_content.js ./pkg
-cp ./service_worker/pkg/service_worker.js ./pkg
-cp ./service_worker/pkg/service_worker_bg.wasm ./pkg
+cp ./service-worker/pkg/service_worker.js ./pkg
+cp ./service-worker/pkg/service_worker_bg.wasm ./pkg
 cp ./content/pkg/content.js ./pkg 
 cp ./content/pkg/content_bg.wasm ./pkg
 cp ./manifest_v3.json ./pkg/manifest.json
