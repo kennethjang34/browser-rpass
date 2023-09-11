9e3c07f69735a910b2769f5cd275babbf3fb2c97
JANG <kennethjang34@gmail.com>
2023-09-11 06:15:55 -0700

    refactor(popup): move init logic to app component

--- popup/src/lib.rs
+++ popup/src/lib.rs
@@ -1,8 +1,13 @@
-use browser_rpass::request::*;
-use browser_rpass::response::{GetResponse, ResponseEnum, ResponseEnumTrait};
-use browser_rpass::util::*;
-use browser_rpass::util::*;
-use event_handlers::request_handlers::{create_response_listener, create_response_process_cb};
+use crate::store::PopupStore;
+#[allow(warnings)]
+pub(crate) use browser_rpass::request::*;
+pub(crate) use browser_rpass::response::{GetResponse, ResponseEnum, ResponseEnumTrait};
+use browser_rpass::use_store;
+pub(crate) use browser_rpass::util::*;
+pub(crate) use browser_rpass::util::*;
+pub(crate) use event_handlers::request_handlers::{
+    create_response_listener, create_response_process_cb,
+};
 use gloo_utils::format::JsValueSerdeExt;
 use js_sys::Map;
 use std::collections::HashMap;
@@ -15,61 +20,15 @@ mod api;
 mod app;
 mod components;
 mod event_handlers;
+mod pages;
+mod router;
 mod store;
-use browser_rpass::log;
-use browser_rpass::store::{DATA_STORAGE, MESSAGE_ACKNOWLEDGEMENTS_POP_UP};
+pub(crate) use browser_rpass::log;
+pub(crate) use browser_rpass::store::{DATA_STORAGE, MESSAGE_ACKNOWLEDGEMENTS_POP_UP};
 
 #[wasm_bindgen(start)]
 pub async fn run_app() -> Result<(), JsValue> {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
-    if let Ok(passphrase) = chrome
-        .storage()
-        .session()
-        .get_string_value("passphrase")
-        .await
-    {
-        if let Some(passphrase) = passphrase {
-            // chrome.storage().session().set(json.clone()).await;
-
-            let port = chrome.runtime().connect();
-            let get_password_request = RequestEnum::create_get_request(
-                "some.website.com".to_owned(),
-                Resource::Password,
-                Some(create_request_acknowledgement()),
-                None,
-                // Some(header.clone()),
-            );
-            let get_username_request = RequestEnum::create_search_request(
-                "some.website.com".to_owned(),
-                Resource::Username,
-                Some(create_request_acknowledgement()),
-                None,
-                // Some(header.clone()),
-            );
-            let on_message_cb = create_response_listener(port.clone());
-            port.on_message()
-                .add_listener(on_message_cb.as_ref().clone());
-            on_message_cb.forget();
-            let ctx = HashMap::new();
-            MESSAGE_ACKNOWLEDGEMENTS_POP_UP.lock().unwrap().insert(
-                get_password_request.get_acknowledgement().clone().unwrap(),
-                create_response_process_cb(get_password_request.clone(), ctx),
-            );
-            let ctx = HashMap::new();
-            MESSAGE_ACKNOWLEDGEMENTS_POP_UP.lock().unwrap().insert(
-                get_username_request.get_acknowledgement().clone().unwrap(),
-                create_response_process_cb(get_username_request.clone(), ctx),
-            );
-            port.post_message(
-                <JsValue as JsValueSerdeExt>::from_serde(&get_password_request).unwrap(),
-            );
-            port.post_message(
-                <JsValue as JsValueSerdeExt>::from_serde(&get_username_request).unwrap(),
-            );
-        } else {
-            log!("passphrase is not set");
-        }
-    }
     yew::Renderer::<app::App>::new().render();
     Ok(())
 }
