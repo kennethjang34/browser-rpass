cb23ac96570d39959856669b3923912464a9e4ee
Junhyeok Jang <kennethjang34@gmail.com>
2023-11-12 09:58:16 -0800

    feat: add edit button for account editing

--- popup/src/components/account_entry_list.rs
+++ popup/src/components/account_entry_list.rs
@@ -2,6 +2,7 @@ use std::rc::Rc;
 
 use crate::Account;
 use crate::Resource;
+use log::*;
 use yew;
 use yew::prelude::*;
 
@@ -26,6 +27,16 @@ pub fn account_entry_list_component(props: &AccountEntryListProps) -> Html {
         })
     };
     let delete_account2 = delete_account.clone();
+    let edit_account = {
+        Callback::<(MouseEvent, Rc<Account>)>::from({
+            move |(e, account): (MouseEvent, Rc<Account>)| {
+                e.prevent_default();
+                let id = account.id.clone();
+                debug!("edit account: {:?}", account);
+                // delete_resource(id.clone(), Resource::Account);
+            }
+        })
+    };
     let account_list_component = props
         .accounts
         .iter()
@@ -33,16 +44,27 @@ pub fn account_entry_list_component(props: &AccountEntryListProps) -> Html {
         .enumerate()
         .map(|(i, account)| {
             let delete_account2 = delete_account2.clone();
+            let edit_account = edit_account.clone();
             let id = account.id.clone();
+            let account2 = account.clone();
             html! {
                 <tr key={id.clone()} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                     <AccountEntry id={i} account={account.clone()}></AccountEntry>
                     <td class="px-1 py-0.5">
                     <a href="#" 
+                    onclick={move |e:MouseEvent|{
+                        edit_account.emit(
+                                (
+                        e,account.clone())
+                    )}}
+                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline">{ "Edit" }</a>
+                    </td>
+                    <td class="px-1 py-0.5">
+                    <a href="#" 
                     onclick={move |e:MouseEvent|{delete_account2.emit(
                                 (
-                                e,account.clone())
-                                )}}
+                                e,account2.clone())
+                    )}}
                     class="font-medium text-blue-600 dark:text-blue-500 hover:underline">{ "Delete" }</a>
                     </td>
                 </tr>
