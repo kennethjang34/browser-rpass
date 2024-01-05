631d09e62757260364e14e48922cc9bc69a03ffd
a <a@gmail.com>
2024-01-04 19:55:50 -0800

    chore: remove unnecessary logs

--- run_content.js
+++ run_content.js
@@ -1,8 +1,6 @@
 (async () => {
   const src = chrome.runtime.getURL("content.js");
-  console.log("init wasm");
   const wasm = await import(src);
-  console.log("wasm script imported");
   try {
     await wasm.default();
     console.log("wasm script executed successfully");
@@ -10,6 +8,6 @@
     //wasm script execution failed. Most likely due to some security policy.
     //Do not throw error, just log.
     //Users will not be able to use suggestion dropdowns but popup will still work.
-    console.log(err);
+    console.error(err);
   }
 })();
