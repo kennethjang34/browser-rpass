93678d2721ea6d9d774c9a06900d35b2250e34cd
a <a@gmail.com>
2024-01-04 18:46:44 -0800

    build: build script path fix

--- build-dev.sh
+++ build-dev.sh
@@ -1,9 +1,55 @@
+#!/bin/bash
+
+# echo "Which browser version do you want? (chrome/chromium)"
+# read browser
+#
+# echo "Enter your extension ID:"
+# read extension_id
+#
+# manifest_path=""
+# manifest='
+# {
+#     "name": "com.rpass",
+#     "description": "rpass",
+#     "path": "'$HOME'/rpass/browser-rpass/target/debug/native-client",
+#     "type": "stdio",
+#     "allowed_origins": ["chrome-extension://'$extension_id'/"]
+# }
+# '
+#
+# if [ "$browser" = "chrome" ]; then
+#     if [ "$(uname)" = "Darwin" ]; then
+#         manifest_path="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts/com.rpass.json"
+#     elif [ "$(uname)" = "Linux" ]; then
+#         manifest_path="$HOME/.config/google-chrome/NativeMessagingHosts/com.rpass.json"
+#     else
+#         echo "Unsupported platform"
+#         exit 1
+#     fi
+# elif [ "$browser" = "chromium" ]; then
+#     if [ "$(uname)" = "Darwin" ]; then
+#         manifest_path="$HOME/Library/Application Support/Chromium/NativeMessagingHosts/com.rpass.json"
+#     elif [ "$(uname)" = "Linux" ]; then
+#         manifest_path="$HOME/.config/chromium/NativeMessagingHosts/com.rpass.json"
+#     else
+#         echo "Unsupported platform"
+#         exit 1
+#     fi
+# else
+#     echo "Unsupported browser"
+#     exit 1
+# fi
+#
+# echo "Manifest path: $manifest_path"
+# touch "$manifest_path"
+#
+# echo "$manifest" > "$manifest_path"
 rm -rf ./pkg
 mkdir pkg
 cargo build -p native-client
 (cd ./popup && trunk build ./index.html --dist ./pkg)
-wasm-pack build ./service-worker --target web --out-dir ./pkg
-wasm-pack build ./content --target web --out-dir ./pkg
+wasm-pack build ./service-worker  --target web --out-dir ./pkg --dev
+wasm-pack build ./content --target web --out-dir ./pkg --dev
 cp -r ./service-worker/pkg/* ./pkg
 cp -r ./content/pkg/* ./pkg
 cp -r ./popup/pkg/* ./pkg
--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 rm -rf ./pkg
 mkdir pkg
-(cd ./native-client && cargo build)
+(cd ./native-client && cargo build --release)
 (cd ./service-worker && source ./build.sh)
 (cd ./content && source ./build.sh)
 (cd ./popup && source ./build.sh)
@@ -8,3 +10,52 @@ mv ./service-worker/pkg/* ./pkg
 mv ./popup/pkg/* ./pkg
 mv ./content/pkg/* ./pkg
 cp ./manifest_v3.json ./pkg/manifest.json
+
+echo "Which browser version do you want? (chrome/chromium)"
+read browser
+
+echo "Enter your extension ID:"
+read extension_id
+
+manifest_path=""
+manifest='
+{
+    "name": "com.rpass",
+    "description": "rpass",
+    "path": "'$HOME'/rpass/browser-rpass/target/release/native-client",
+    "type": "stdio",
+    "allowed_origins": ["chrome-extension://'$extension_id'/"]
+}
+'
+
+if [ "$browser" = "chrome" ]; then
+    if [ "$(uname)" = "Darwin" ]; then
+        manifest_path="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts/com.rpass.json"
+    elif [ "$(uname)" = "Linux" ]; then
+        manifest_path="$HOME/.config/google-chrome/NativeMessagingHosts/com.rpass.json"
+    else
+        echo "Unsupported platform"
+        exit 1
+    fi
+elif [ "$browser" = "chromium" ]; then
+    if [ "$(uname)" = "Darwin" ]; then
+        manifest_path="$HOME/Library/Application Support/Chromium/NativeMessagingHosts/com.rpass.json"
+    elif [ "$(uname)" = "Linux" ]; then
+        manifest_path="$HOME/.config/chromium/NativeMessagingHosts/com.rpass.json"
+    else
+        echo "Unsupported platform"
+        exit 1
+    fi
+else
+    echo "Unsupported browser"
+    exit 1
+fi
+
+echo "Manifest path: $manifest_path"
+
+touch "$manifest_path"
+echo "$manifest" > "$manifest_path"
+
+
+
+
